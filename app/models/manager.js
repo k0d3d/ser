2b88ebfaa1c142c2e1e7e90277ab95b16684f5ec
Michael Rhema <michael.rhema@gmail.com>
2014-03-22 12:35:05 +0100

    moving off master

---
+++ app/models/manager.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+
+
+ManagerSchema = new Schema({
+  userId: {type: Schema.ObjectId, ref: 'User'},
+  name : {type: String},
+  coverage: {type: String},
+  position: {type: String},
+  employer: [{type: Schema.ObjectId}],
+  summary: {type: String},
+  twitter: {type: String},
+  facebook: {type: String},
+  address: {type: String},
+  phone: {type: String},
+  image: {type: String}
+});
+
+mongoose.model('Manager', ManagerSchema);
+
+module.exports = mongoose.model('Manager');
