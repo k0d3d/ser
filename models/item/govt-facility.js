b48e6332b92550fdd26a39b46124d13879effab2
Michael Rhema <michael.rhema@gmail.com>
2014-04-11 22:15:15 +0100

    pushing to heroku..forgot to git add .

---
+++ models/item/govt-facility.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var GovtFacSchema = new Schema({
+  lga: {type: String},
+  lga_ward: {type: String},
+  facilityName: {type: String},
+  facilityType: {type: String},
+  ownership: {type: String},
+  stateCode: {type: Number},
+  lgaCode: {type: Number},
+  facilityTypeCode: {type: Number},
+  ownershipCode: {type: Number},
+  facilityNoCode: {type: String}
+});
+
+GovtFacSchema.statics = {
+  /**
+  * Auto Complete
+  * @param {regex} itemName
+  * @param {function} cb
+  * @api private
+  */
+  autocomplete: function(name, field, cb) {
+    var wit = this.find({}).limit(20);
+    wit
+    .distinct('', function (err, i) {
+      
+    })
+    .regex('productName',new RegExp(name, 'i'))
+    .exec(cb);
+    //wit.exec(cb);
+  }
+};
+
+mongoose.model('govtfacility', GovtFacSchema);
+module.exports = mongoose.model('govtfacility');
