9d4796ef7321e1ed85e74848c937bebad4ecf833
Mykel Koded <michael.rhema@gmail.com>
2014-07-15 13:32:56 +0100

    add to drugstoc list done

---
+++ controllers/admin/items.js
@@ -0,0 +1,34 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Item = require('../../models/item.js').Drug,
+    util = require('util');
+
+
+module.exports.routes = function(app, login){
+  var item = new Item();
+
+  app.get('/x/drugs', login.ensureLoggedIn(), function(req, res){
+      res.render('index',{
+        title: 'Admin :: All Orders'
+      });
+    }
+  );
+
+  app.get('/api/internal/admin/items',function(req, res){
+
+    item.queryAdminDrugs(req.query)
+    .then(function(r){
+        res.json(200, r);
+    }, function (err) {
+      res.json(400, err);
+    });
+  });
+
+
+
+
+
+};
